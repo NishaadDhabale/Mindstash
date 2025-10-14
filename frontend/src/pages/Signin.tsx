fd94ed8e330ce4461ae97d54aa66c50b19f1f28b
ninu <nishaad.dhabale@gmail.com>
2025-10-15 04:35:34 +0530

    bug input space fix

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -34,7 +34,7 @@ export function Signin() {
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white rounded-xl border min-w-48 p-8">
         <Input reference={usernameRef} type="text" placeholder="Username" />
-        <Input reference={passwordRef} type="password "placeholder="Password" />
+        <Input reference={passwordRef} type="password" placeholder="Password" />
         <div className="flex justify-center pt-4">
           <Button
             onClick={signin}
